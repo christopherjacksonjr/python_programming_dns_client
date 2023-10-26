021b00923bbacefd6f824d6e099ec6e3a586cbf7
Christopher Jackson <jacksonchristopher0924@gmail.com>
2023-10-25 23:42:03 -0500

    Added local dns server and public dns server

--- DNSClient.py
+++ DNSClient.py
@@ -1,8 +1,8 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = ????
-real_name_server = ???? # Research public DNS servers to find a valid DNS server IP address to use
+local_host_ip = '71.10.216.1'
+real_name_server = '8.8.8.8' # Research public DNS servers to find a valid DNS server IP address to use
 
 
 # Create a list of domain names to query - use the same list from the DNS Server
@@ -12,7 +12,7 @@ domainList  = ['example.com.','safebank.com','google.com','nyu.edu','legitsite.c
 def query_local_dns_server(domain,question_type):
     resolver = dns.resolver.Resolver()
     resolver.nameservers = [????]
-    answers = resolver.resolve(????, ????) # provide the domain and question_type
+    answers = resolver.resolve(domain, question_type) # provide the domain and question_type
 
     ip_address = answers[??].to_text()
     return ip_address   
